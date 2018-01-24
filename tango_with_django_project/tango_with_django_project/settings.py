2c1703b002a0b17add9ac0c1a7ed174aff306983
James Elner <Jelner@rocketmail.com>
2018-01-24 18:04:45 +0000

    start of chapter 5

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -121,10 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_PATH = os.path.join(BASE_DIR,'static')
 
-STATICFILES_DIR = [STATIC_DIR, ]
+STATIC_URL = '/static/' # You may find this is already defined as such.
 
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
 
 # Media files
 
